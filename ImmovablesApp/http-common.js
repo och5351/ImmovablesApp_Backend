7ca206cc673da2f898a0a8df3cc08bed782c21df
och5351 <dhcksgo5319@naver.com>
2020-09-08 16:01:12 +0900

    git push

--- ImmovablesApp/http-common.js
+++ ImmovablesApp/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://13.125.30.205:3000/api",
-  baseURL: "http://192.168.0.14:3000/api",
+  baseURL: "http://13.125.247.97/api",
    //baseURL: "http://192.168.25.3:3000/api",
   headers: {    "Content-type": "application/json"
   }

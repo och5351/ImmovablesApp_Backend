51568ed67ab7946533e4233b9b0c2d9563624b5c
och5351 <dhcksgo5319@naver.com>
2020-09-10 20:03:39 +0900

    Change IP

--- ImmovablesApp/http-common.js
+++ ImmovablesApp/http-common.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://13.125.30.205:3000/api",
-  baseURL: "http://13.125.247.97/api",
-   //baseURL: "http://192.168.25.3:3000/api",
+  baseURL: "http://54.180.144.184/api",
   headers: {    "Content-type": "application/json"
   }
 });

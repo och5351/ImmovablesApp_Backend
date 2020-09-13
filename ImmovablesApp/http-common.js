1b566ede0d5bbef66beb6ec50b4df9fe430a4391
och5351 <dhcksgo5319@naver.com>
2020-09-13 15:17:52 +0900

    Changing port

--- ImmovablesApp/http-common.js
+++ ImmovablesApp/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://54.180.144.184/api",
+  baseURL: "http://54.180.109.57:3001/api",
   headers: {    "Content-type": "application/json"
   }
 });

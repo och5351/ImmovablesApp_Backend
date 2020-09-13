379de685fbc146a1cbd63c73f948da49b0df48ba
och5351 <dhcksgo5319@naver.com>
2020-09-13 17:52:35 +0900

    Delete dataCrawler and dumpfile

--- dataCrawling/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\och5351\Desktop\github_och\ImmovablesApp\dataCrawling\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

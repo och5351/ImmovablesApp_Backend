95a6ef1d47b4ae0a3edf56d7ab129fe0d8c536b8
och5351 <dhcksgo5319@naver.com>
2020-09-13 15:51:37 +0900

    delete critical file

--- ImmovablesApp/http-common.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-/* build test */
-export default axios.create({
-  baseURL: "http://54.180.109.57:3000/api",
-  headers: {    "Content-type": "application/json"
-  }
-});

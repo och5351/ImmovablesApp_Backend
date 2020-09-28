d10b8f6c50b9cc60f09540fd809f31e3798654e2
Changgi Kang <elien940318@gmail.com>
2020-09-28 18:17:40 +0900

    login oauth check

--- routes/Login.js
+++ routes/Login.js
@@ -5,6 +5,40 @@ var router = express.Router();
 var dbConObj = require('../lib/dbConnector');
 var conn = dbConObj.init();
 
+
+// 유저가 현재 Immovables.uesr 테이블에 존재하는지 체크
+router.post('/checkUser', function(req, res) {  
+  var data = [ req.body._email, req.body._uid, req.body._name];
+  var selectQuery = 'SELECT count(*) as id FROM user WHERE email = ? ';
+  let count = -1;
+  conn.query(selectQuery, data[0], function(err, result) {
+      if(!err) {
+        count = result[0].id;
+      }
+      else{
+        console.log(err);
+      }
+  });
+  // 회원정보가 db에 저장되어있지 않으면 추가해줍니다.
+  if (count == 0){    
+    var insertQuery = 'INSERT INTO user(email, uid, name) VALUES (?, ?, ?)';
+    conn.query(insertQuery, [data[0], data[1], data[2]], function(err){
+      if(err){
+        console.log(err);
+      }
+    });
+  }
+  // 회원정보가 db에 저장되어있다면 update 해줍니다 (이름. uid는 고정값이라 변동이 없습니다.).
+  else{
+    var updateQuery = 'UPDATE user SET name=? WHERE email=?';
+    conn.query(updateQuery, [data[2], data[0]], function(err){
+      if(err){
+        console.log(err);
+      }
+    });
+  }
+});
+
 router.post('/chinfo', function(req, res){
   var data = [req.body.id,req.body.pw];
   var sql = "SELECT count(*) as id FROM test WHERE id = ? ";

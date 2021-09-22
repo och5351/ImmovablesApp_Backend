a46c085d5892e7c1d0002d3baddc313cc2df64a9
och5351 <dhcksgo5319@naver.com>
2021-09-23 00:24:24 +0900

    jenkins shell test 4(nodejs install)

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,7 +9,7 @@ node {
  
     // mvn 툴 선언하는 stage, 필자의 경우 maven 3.6.0을 사용중
     stage('Ready'){  
-        bat "choco -?"
+        bat "choco install nodejs-lts"
         // sh "echo 'Ready to build'"
         // mvnHome = tool 'Maven 3.6.0'
     }

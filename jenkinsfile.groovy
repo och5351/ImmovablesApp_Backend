f9751ff01fda1bfbcc3af04d29c43465f6de3c83
och5351 <dhcksgo5319@naver.com>
2021-09-24 14:53:56 +0900

    jenkins server load test

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -42,6 +42,7 @@ node {
         bat "docker rmi registry.hub.docker.com/${dockerId}/${dockerRepo}:${env.BUILD_NUMBER}"
     }
      
+     
     // kubernetes에 배포하는 stage, 배포할 yaml파일(필자의 경우 test.yaml)은 jenkinsfile과 마찬가지로 git소스 root에 위치시킨다.
     // kubeconfigID에는 앞서 설정한 Kubernetes Credentials를 입력하고 'sh'는 쿠버네티스 클러스터에 원격으로 실행시킬 명령어를 기술한다.
     stage('Kubernetes deploy') {

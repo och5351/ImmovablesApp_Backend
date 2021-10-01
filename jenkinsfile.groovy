177e960d8bd67f676a4a003e5bdd2e3342603256
och5351 <dhcksgo5319@naver.com>
2021-10-01 16:41:58 +0900

    fff

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -88,7 +88,7 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
     ),
     containerTemplate(
       name: 'kubectl', 
-      image: 'bitnami/kubectl:1..5', 
+      image: 'bitnami/kubectl:1.21.5', 
       command: 'cat', 
       ttyEnabled: true
     ),

97e8eea26403386e6fafb944e95482efcb1f55f4
och5351 <dhcksgo5319@naver.com>
2021-10-02 09:44:12 +0900

    checkcout

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -110,13 +110,14 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
         
         stage('Clone repository') {
             container('git') {
-                https://gitlab.com/gitlab-org/gitlab-foss/issues/38910
-                checkout([$class: 'GitSCM',
-                    branches: [[name: '*/develop']],
-                    userRemoteConfigs: [
-                        [url: 'https://github.com/och5351/ImmovablesApp_Backend.git']//, credentialsId: 'gitlab-account']
-                    ],
-                ])
+                // https://gitlab.com/gitlab-org/gitlab-foss/issues/38910
+                // checkout([$class: 'GitSCM',
+                //     branches: [[name: '*/develop']],
+                //     userRemoteConfigs: [
+                //         [url: 'https://github.com/och5351/ImmovablesApp_Backend.git']//, credentialsId: 'gitlab-account']
+                //     ],
+                // ])
+                checkout scm
             }
         }
         

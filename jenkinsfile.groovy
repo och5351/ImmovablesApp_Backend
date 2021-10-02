e9150b355bd1b97eff15667a7fbe823fbe708465
och5351 <dhcksgo5319@naver.com>
2021-10-03 01:25:35 +0900

    last test

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -74,7 +74,7 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
       try {
         // Start
         stage('Start'){
-          // notifyStarted(SLACK_CHANNEL)
+          notifyStarted(SLACK_CHANNEL)
         }
 
         // git clone 스테이지
@@ -93,7 +93,7 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
         }
         // docker image build 스테이지
         stage('docker build') {
-          // notifyDocker(SLACK_CHANNEL)
+          notifyDocker(SLACK_CHANNEL)
           container('docker') {
               app = docker.build("${dockerId}/${dockerRepo}")
           }
@@ -111,7 +111,7 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
         // kubernetes에 배포하는 stage, 배포할 yaml파일(필자의 경우 test.yaml)은 jenkinsfile과 마찬가지로 git소스 root에 위치시킨다.
         // kubeconfigID에는 앞서 설정한 Kubernetes Credentials를 입력하고 'sh'는 쿠버네티스 클러스터에 원격으로 실행시킬 명령어를 기술한다.
         stage('Kubernetes deploy') {
-          notifyDeployment(SLACK_CHANNEL)
+          //notifyDeployment(SLACK_CHANNEL)
           container('kubectl') {
               sh "sed -i.bak 's#DATE_STRING#${DATE}#' ./k8s/Imm-deployment.yaml"
               // sh "kubectl apply -f k8s/"
@@ -120,11 +120,11 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
               sh "kubectl apply -f ./k8s/"
           }
         }
-        // notifySuccessful(SLACK_CHANNEL)
+        notifySuccessful(SLACK_CHANNEL)
       } catch(e) {
         /* 배포 실패 시 */
         currentBuild.result = "FAILED"
-        // notifyFailed(SLACK_CHANNEL)
+        notifyFailed(SLACK_CHANNEL)
     }
   }
 }

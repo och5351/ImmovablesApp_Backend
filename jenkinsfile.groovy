31920be889a29006121f461d9cb2581ca12020d4
och5351 <dhcksgo5319@naver.com>
2021-10-17 20:43:52 +0900

    test

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -73,9 +73,9 @@ podTemplate(label: 'jenkins-slave-pod',  //jenkins slave pod name
         // gradle build 스테이지
         stage('Gradle build') {
           container('gradle') {
-              sh "cd springjava"
+              sh "mv ./springjava/* ./"
               sh "ls"
-              sh "./springjava/gradlew build"
+              sh "./gradlew build"
           }
         }
         // // docker image build 스테이지

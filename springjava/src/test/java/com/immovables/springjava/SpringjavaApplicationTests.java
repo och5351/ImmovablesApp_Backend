4328e229d0fccf1dcdf561baf3a1ba12679e5a1c
och5351 <dhcksgo5319@naver.com>
2021-12-21 20:02:18 +0900

    테스트 주석처리

--- springjava/src/test/java/com/immovables/springjava/SpringjavaApplicationTests.java
+++ springjava/src/test/java/com/immovables/springjava/SpringjavaApplicationTests.java
@@ -3,7 +3,7 @@ package com.immovables.springjava;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpringjavaApplicationTests {
 
 	@Test
